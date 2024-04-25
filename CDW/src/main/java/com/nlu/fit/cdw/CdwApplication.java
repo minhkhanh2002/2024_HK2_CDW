52dbcf5d8945cb096be4cd213551635ff1e01ffc
minhkhanh2002 <ngominhkhanh2002@gmail.com>
2024-04-25 20:38:29 +0700

    first commit

---
+++ CDW/src/main/java/com/nlu/fit/cdw/CdwApplication.java
@@ -0,0 +1,13 @@
+package com.nlu.fit.cdw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdwApplication.class, args);
+	}
+
+}
---
+++ CDW/src/test/java/com/nlu/fit/cdw/CdwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nlu.fit.cdw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
